047d0b8d6ee593b6e066a97516afc448cc9613b9
GuillaumeCARRA <carraguillaume@gmail.com>
2022-12-21 17:01:46 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    //variable d'instance
+    //public static int num = 7;
+    public static void main(String[] args) {
+        //var locale
+        /*int data = 10;
+        System.out.println("Hello World");
+        System.out.println(num);*/
+        boolean isOpen = true;
+        System.out.println(isOpen);
+
+        byte num = 1;
+        System.out.println(num);
+
+        short shortNum = 5;
+        System.out.println(shortNum);
+
+        int num2 = 7;
+
+        long longNum = 232;
+
+        float floatNum = 7.4f;
+
+        double doubleNum = 14;
+
+        char charNum = 1;
+
+        System.out.println(num2);
+        System.out.println(longNum);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+        System.out.println(charNum);
+    }
+}
+
